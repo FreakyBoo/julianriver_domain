1ac1411ea5d860644377600272ba36306aa6138a
Freakyboo <julianriversandvikendelgado@gmail.com>
2025-02-11 10:21:35 +0000

    Etter innlogging, vil brukeren bli sendt til welcome.php, hvor de får en personlig velkomstmelding sammen med lenker til å gjøre bestillinger eller logge ut.

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<?php
+session_start(); // Start sesjonen for å få tilgang til brukerinformasjon
+
+// Sjekk om brukeren er logget inn
+if (!isset($_SESSION['kunde_id'])) {
+    header("Location: login.php"); // Hvis ikke logget inn, omdiriger til login-siden
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>Velkommen</title>
+</head>
+<body>
+    <h1>Velkommen, <?php echo $_SESSION['fornavn']; ?>!</h1>
+    <p>Du er nå logget inn på nettbutikken.</p>
+    <a href="order.php">Bestill produkter</a> | 
+    <a href="logout.php">Logg ut</a>
+</body>
+</html>
